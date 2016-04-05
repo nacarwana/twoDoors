58f64ce41e3613a5af77cd3309f748a3f2542afa
nacarwana <nac_carwana@hotmail.com>
2016-04-05 15:23:08 -0400

    twoDoors.c
    Assignment 2

---
+++ twoDoors.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+int main(int argc, const char * argv[]) {
+    
+    char play;
+    int choice;
+    char answer[150];
+    int keepPlaying = 1;
+    
+    printf("Welcome to Two doors.\n");
+    printf("Would you like to play? (y/n): ");
+    
+    while (keepPlaying == 1) {
+        
+        scanf(" %c", &play);
+        
+        if (play == 'y') {
+            
+            keepPlaying = 1;
+            
+            printf("\nYou are a prisoner in a room with 2 doors and 2 guards.\n");
+            printf("One of the doors will guide you to freedom and behind the other is a hangman --you don't know which is which.\n");
+            printf("One of the guards always tells the truth and the other always lies. You don't know which one is the truth-teller or the liar either.\n");
+            printf("You have to choose and open one of these doors, but you can only ask a single question to one of the guards.\n");
+            printf("What do you ask so you can pick the door to freedom?\n\n");
+            printf("\t1.Ask the truth-guard to point to the door of doom.\n");
+            printf("\t2.Ask the liar-guard to point to the door of doom.\n");
+            printf("\t3.Doesn't matter which one you pick.\n");
+            scanf(" %d", &choice);
+            
+            strcpy(answer, "No matter which one you choose the guards both tell you which door leads to death, and therefore you can pick the other door.\n");
+            switch (choice) {
+                case 1:
+                    printf("%s\n", answer);
+                    break;
+                case 2:
+                    printf("%s\n", answer);
+                    break;
+                case 3:
+                    printf("%s\n", answer);
+                    break;
+                default:
+                    break;
+            }
+            
+            printf("Would you like to play again? (y/n): \n");
+            
+        } else if (play != 'y'){
+            
+            keepPlaying = 0;
+            
+        }
+        
+    }
+    
+    return 1;
+}
